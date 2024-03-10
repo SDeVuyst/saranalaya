5bff208f1797f09644394c586bb09d6644de3a0c
Silas De Vuyst <silasdevuyst@hotmail.com>
2024-03-10 12:34:33 +0100

    removed static from urlpatterns

--- saranalaya/urls.py
+++ saranalaya/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.site.urls),
     path('admin_app', include("admin_app.urls"))
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+]
 
 admin.site.site_header = "Saranalaya Admin"
 admin.site.site_title = "Saranalaya Admin Portal"
