b844fb615b495ef7cf984e6ec3e08d082b703816
Silas De Vuyst <silasdevuyst@hotmail.com>
2024-03-10 11:36:17 +0100

    testing allowed hosts

--- saranalaya/settings.py
+++ saranalaya/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.86.200']
+ALLOWED_HOSTS = ['192.168.86.200', '0.0.0.0', 'localhost']
 
 
 # Application definition
