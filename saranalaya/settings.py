5c55738342d1726a1a6cf88bc4c4bb262d7db386
Silas De Vuyst <silasdevuyst@hotmail.com>
2024-03-10 12:13:28 +0100

    added vanakaam.be to allowed hosts & turned off debug

--- saranalaya/settings.py
+++ saranalaya/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.86.200', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.86.200', '0.0.0.0', 'localhost', '127.0.0.1','vanakaam.be']
 
 
 # Application definition
