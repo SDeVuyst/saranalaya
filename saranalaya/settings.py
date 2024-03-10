40f819c5313c8917dd3672166bb238634bb42614
Silas De Vuyst <silasdevuyst@hotmail.com>
2024-03-10 11:31:44 +0100

    updated allowed hosts

--- saranalaya/settings.py
+++ saranalaya/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=p1%xu35&6-^(o-#fyx!p-9op=uo&)i0s(!7i8of#=+41mc3x%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.86.200']
 
 
 # Application definition
