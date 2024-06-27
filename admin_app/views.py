65e7d05c0c2e8b1ebed2984f216ceb74229586f4
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2024-06-27 10:46:54 +0200

    Fixed yearly donations chart

--- admin_app/views.py
+++ admin_app/views.py
@@ -4,7 +4,6 @@ from django.db.models import Sum
 from django.db.models.functions import ExtractYear
 
 from admin_app.models import Donation
-from admin_app.utils.charts import months, colorPrimary, colorSuccess, colorDanger, generate_color_palette, get_year_dict
 
 
 @staff_member_required
@@ -20,21 +19,25 @@ def get_filter_options(request):
 @staff_member_required
 def donations_per_year_chart(request):
 
+    valid_years = request.GET.getlist('years', None)
+    valid_years = [year.replace('/', '') for year in valid_years]
+
     donations = Donation.objects.all()
-    years = donations.values("date__year").distinct()
-    years = [y['date__year'] for y in years]
+    years = donations.values("date__year").order_by("date__year").distinct()
+    years = [str(y['date__year']) for y in years]
+
+    # make sure only the correct years are selected
+    valid_years = years if valid_years is None else valid_years
+    years = list(filter(lambda year: year in valid_years, years))
 
     return JsonResponse({
-        "title": f"Donations per year",
         "data": {
             "labels": [str(year) for year in years],
             "datasets": [{
                 "label": "Amount (€)",
-                "backgroundColor": [colorSuccess, colorDanger],
-                "borderColor": [colorSuccess, colorDanger],
                 "data": [
-                    donations.filter(date__year=y).aggregate(Sum("amount")) for y in years
+                    donations.filter(date__year=y).aggregate(Sum("amount"))['amount__sum'] for y in years
                 ],
             }]
-        },
+        }
     })
