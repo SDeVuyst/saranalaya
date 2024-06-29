f3fa9313abb5821e7955d74293ceffc162a43524
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2024-06-29 20:53:22 +0200

    fixed bug that models would not get translated

--- admin_app/models.py
+++ admin_app/models.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.shortcuts import resolve_url
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 import os
 
