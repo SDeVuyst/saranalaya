01af9728fa1f39df2520d61dc5f7bac35e3558fa
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2024-07-15 20:18:20 +0200

    work on homepage

--- saranalaya/settings.py
+++ saranalaya/settings.py
@@ -162,6 +162,9 @@ LANGUAGES = [
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
