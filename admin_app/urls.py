724ed46eb3a04db42191b227c50c8cfbac15ad34
SDeVuyst <silasdevuyst@hotmail.com>
2025-03-02 20:21:33 +0100

    added kind + niews

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('kinderen/', views.kinderen, name="kinderen"),
+    path('nieuws/', views.nieuws, name="nieuws"),
 ]
--- admin_app/views.py
+++ admin_app/views.py
@@ -1,7 +1,6 @@
 import json
+from .models import Child, News
 from django.db.models import Sum
-from django.http import HttpResponse
-from django.utils.html import escape
 from django.utils.translation import gettext as _
 from django.template.response import TemplateResponse
 from .utils.helper import percentage_change
@@ -13,6 +12,21 @@ from .utils.helper import *
 
 
 def index(request):
+    kinderen = Child.objects.all().order_by('-date_of_admission')[:4]
+    nieuws = News.objects.all().order_by('-last_updated')[:4]
+    context = {
+        'kinderen': kinderen,
+        'nieuws': nieuws
+    }
+
+    return TemplateResponse(request, "pages/index.html", context)
+
+def kinderen(request):
+    context = {}
+
+    return TemplateResponse(request, "pages/index.html", context)
+
+def nieuws(request):
     context = {}
 
     return TemplateResponse(request, "pages/index.html", context)
@@ -157,69 +171,3 @@ def dashboard_callback(request, context):
     )
 
     return context
-
-
-def generate_mailto_link(request):
-    # Get email addresses from the query parameters
-    email_addresses = request.GET.get('emails', '')
-    
-    # Escape email addresses to ensure safety
-    escaped_emails = escape(email_addresses)
-    
-    # Split email addresses into a list
-    email_list = escaped_emails.split(',')
-    
-    # Define a maximum number of addresses per mailto link
-    MAX_EMAILS_PER_LINK = 50
-    
-    # Split the email addresses into smaller groups
-    email_groups = [email_list[i:i + MAX_EMAILS_PER_LINK] for i in range(0, len(email_list), MAX_EMAILS_PER_LINK)]
-    
-    # Generate buttons for each batch of email links
-    buttons_html = ""
-    for i, group in enumerate(email_groups):
-        mailto_link = f"mailto:?bcc={','.join(group)}?subject=Important Information&body=Please review the following information."
-        buttons_html += f"""
-        <button onclick="window.open('{mailto_link}', '_blank')">Open Batch {i + 1}</button><br>
-        """
-    
-    # Generate the HTML content
-    html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Send Emails</title>
-        <style>
-            body {{
-                font-family: Arial, sans-serif;
-                text-align: center;
-                padding: 20px;
-            }}
-            button {{
-                display: inline-block;
-                padding: 10px 20px;
-                font-size: 16px;
-                color: #fff;
-                background-color: #007bff;
-                border: none;
-                border-radius: 5px;
-                text-decoration: none;
-                cursor: pointer;
-                margin: 5px;
-            }}
-            button:hover {{
-                background-color: #0056b3;
-            }}
-        </style>
-    </head>
-    <body>
-        <p>Click on the buttons below to open each batch of email addresses:</p>
-        {buttons_html}
-        <p><a href="javascript:history.back()" class="button">Go Back</a></p>
-    </body>
-    </html>
-    """
-    
-    return HttpResponse(html_content, content_type='text/html')
-
-
